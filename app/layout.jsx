01d8b0579c2eed1922e543981f20bc513628da85
Rohit Gupta <rohitgupta@Rohits-MacBook-Air.local>
2023-11-06 23:56:25 +0530

    fixed the scrollbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -49,8 +49,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-      <html lang="en" className=' overflow-hidden scroll-smooth'>
-        <body className={`${lato.variable} ${raleway.variable} h-[calc(100vh_-_62px)] pb-[58px] no-scrollbar overflow-x-hidden overflow-y-scroll  bg-app`}>
+      <html lang="en" className=' scroll-smooth'>
+        <body className={`${lato.variable} ${raleway.variable} min-h-screen no-scrollbar   bg-app`}>
           <NextTopLoader color="#ac2323"
             initialPosition={0.08}
             crawlSpeed={200}
@@ -63,7 +63,7 @@ export default function RootLayout({ children }) {
           />
           <ReduxProvider>
             <ToastProvider >
-              {children}
+              <main className='min-h-screen'>{children}</main>
               <Footer />
             </ToastProvider>
           </ReduxProvider>
